902512adc7288aecc5fa83418f33ce90c0a45709
gjdim <gjdimpter@gmail.com>
2023-03-30 21:48:30 -0400

    Initial commit

---
+++ src/main/java/com/github/gavvydizzle/inventorybugtester/InventoryBugTester.java
@@ -0,0 +1,17 @@
+package com.github.gavvydizzle.inventorybugtester;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InventoryBugTester extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
